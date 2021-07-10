e646551bc443d672d4f9f08d3ec732c47c4748a1
Ciccio-tech <84313734+Ciccio-tech@users.noreply.github.com>
2021-07-10 12:57:20 +0100

    Aggiunto: RicercaServlet Modificato: ProdottoDAO

--- src/main/java/model/ProdottoDAO.java
+++ src/main/java/model/ProdottoDAO.java
@@ -85,6 +85,33 @@ public class ProdottoDAO {
         }
     }
 
+    public ArrayList<Prodotto> doRetrieveByModelloorMarca(String against, int offset, int limit) throws SQLException {
+        try(Connection c= ConPool.getConnection()){
+            QueryBuilder queryBuilder= new QueryBuilder("prodotto","product");
+            queryBuilder.select("codP, tipo, marca, modello, prezzo, quantità;").where("MATCH(marca, modello) AGAINST(?) LIMIT ?, ?");
+            PreparedStatement ps = c.prepareStatement(queryBuilder.GenerateQuery());
+            ps.setString(1, against);
+            ps.setInt(2, offset);
+            ps.setInt(3, limit);
+            ResultSet rs = ps.executeQuery();
+            ArrayList<Prodotto> prodotti= new ArrayList<>();
+            while(rs.next()){
+                Prodotto p= new Prodotto();
+                p.setCodP(rs.getInt(1));
+                p.setTipo(rs.getString(2));
+                p.setMarca(rs.getString(3));
+                p.setModello(rs.getString(4));
+                p.setPrezzo(rs.getFloat(5));
+                p.setQuantita(rs.getInt(6));
+                prodotti.add(p);
+            }
+            return prodotti;
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
     //aggiungere il doSave, doUpdate e doDelete.
 
---
+++ src/main/java/servlet/RicercaServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+
+import model.Prodotto;
+import model.ProdottoDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet("/Ricerca")
+public class RicercaServlet {
+
+    private final ProdottoDAO prodottoDAO= new ProdottoDAO();
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, SQLException {
+        ArrayList<Prodotto> prodotti = prodottoDAO.doRetrieveByModelloorMarca(request.getParameter("ricerca"), 0, 10);
+        request.setAttribute("prodotti", prodotti);
+        RequestDispatcher requestDispatcher= request.getRequestDispatcher("WEB-INF/ricerca.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, SQLException {
+        doGet(request, response);
+    }
+
+}
