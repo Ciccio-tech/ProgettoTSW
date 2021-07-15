b1e7825ac67e06e8bf4b76125dcc813794aa7e16
Ciccio-tech <84313734+Ciccio-tech@users.noreply.github.com>
2021-07-15 10:28:35 +0100

    Login Funzionante, risolto tutti i problemi

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,11 +1,12 @@
 package servlet;
 
+
 import MyException.MyServletException;
 import model.Cliente;
 import model.ClienteDAO;
 
 
-import javax.servlet.RequestDispatcher;
+//import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,19 +22,22 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username= request.getParameter("username");
         String password= request.getParameter("pass");
-        System.out.println(username + password);
         Cliente cliente= null;
         if(username != null && password!= null){
             cliente = clienteDAO.doRetrieveByUsernamePassword(username, password);
         }
+        else
+            try {
+                throw new MyServletException("username o password non corretti");
+            } catch (MyServletException e) {
+                e.printStackTrace();
+            }
 
+        assert cliente != null;
 
-        System.out.println(cliente.getUsername() + cliente.getPassword());
-
-        request.getSession().setAttribute("cliente", cliente);
-        RequestDispatcher requestDispatcher= request.getRequestDispatcher("../../webapp/index.jsp");
-        requestDispatcher.forward(request, response);
+        response.sendRedirect("index.jsp");
     }
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
