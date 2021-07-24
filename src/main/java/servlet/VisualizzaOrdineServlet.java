445e94a46385a725d114ddab03e041356b8a25b8
Ciccio-tech <84313734+Ciccio-tech@users.noreply.github.com>
2021-07-24 16:51:59 +0100

    Fine Ordine

--- src/main/java/servlet/VisualizzaOrdineServlet.java
+++ src/main/java/servlet/VisualizzaOrdineServlet.java
@@ -6,6 +6,7 @@ import model.OrdineDAO;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,19 +15,22 @@ import java.util.ArrayList;
 
 @WebServlet("/VisualizzaOrdine")
 
-public class VisualizzaOrdineServlet {
+public class VisualizzaOrdineServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         OrdineDAO ordineDAO= new OrdineDAO();
         String username = request.getParameter("username");
         if(username != null) {
             try {
+                System.out.println(username);
                 ArrayList<Ordine> ordini = ordineDAO.doRetrieveByUsername(username);
+                System.out.println(ordini.get(1).getUsername());
                 request.setAttribute("ordini", ordini);
+
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("Ordine.jsp");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("VisualizzaOrdine.jsp");
         requestDispatcher.forward(request, response);
     }
 
